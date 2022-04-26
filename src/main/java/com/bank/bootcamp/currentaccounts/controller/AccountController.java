49a7f198a10817b54e2af1e82b1ba05f498c7c69
Brayan Jenzy La Cruz Carreño <blacruzc@emeal.nttdata.com>
2022-04-26 17:28:45 -0500

    Fix bugs

--- src/main/java/com/bank/bootcamp/currentaccounts/controller/AccountController.java
+++ src/main/java/com/bank/bootcamp/currentaccounts/controller/AccountController.java
@@ -1,7 +1,6 @@
 package com.bank.bootcamp.currentaccounts.controller;
 
 import java.time.LocalDate;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
